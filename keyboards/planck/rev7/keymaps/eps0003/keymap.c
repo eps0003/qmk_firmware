baaf73d3a6122fc35f79be0580ab11a8ec01cce7
epsilon <eps0003@users.noreply.github.com>
2024-02-13 23:25:10 +1100

    Change layer priority and media keys

--- keyboards/planck/rev7/keymaps/eps0003/keymap.c
+++ keyboards/planck/rev7/keymaps/eps0003/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-enum layers { DEF, NUM, MSE, SCR, TXT };
+enum layers { DEF, NUM, TXT, MSE, SCR };
 
 // layers
 #define LA_NUML LT(NUM, KC_TAB)
@@ -42,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  MOD_A,   MOD_S,   MOD_D,   MOD_F,   KC_G,    KC_H,    MOD_J,   MOD_K,   MOD_L,   MOD_SCL, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_ESC,  LA_TXT,  LA_NUML, LA_NUMR, LA_MSE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, KC_ESC,  LA_TXT,  LA_NUML, LA_NUMR, LA_MSE,  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
     ),
 
     [NUM] = LAYOUT_planck_grid(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_GRV,  MOD_1,   MOD_2,   MOD_3,   MOD_4,   KC_5,    KC_6,    MOD_7,   MOD_8,   MOD_9,   MOD_0,   KC_BSLS,
-        KC_CAPS, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_EQL,  KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MRWD, KC_MFFD, KC_MNXT
+        KC_CAPS, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_EQL,  KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, KC_ENT,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     [MSE] = LAYOUT_planck_grid(
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX,
         XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_PGDN, KC_INS,  XXXXXXX, XXXXXXX,
-        _______, _______, _______, _______, _______, _______, _______, LA_MSE2, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
+        _______, _______, _______, _______, _______, _______, _______, LA_MSE2, KC_MPRV, KC_MRWD, KC_MFFD, KC_MNXT
     ),
 
 };
