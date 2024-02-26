fdeb1b07ea62cd9df3158da821d62a317b0c1413
epsilon <eps0003@users.noreply.github.com>
2024-02-26 19:43:43 +1100

    Remove layers from primary thumb keys

--- keyboards/planck/rev7/keymaps/eps0003/keymap.c
+++ keyboards/planck/rev7/keymaps/eps0003/keymap.c
@@ -1,14 +1,15 @@
 #include QMK_KEYBOARD_H
 #include "print.h"
 
-enum layers { DEF, NUM, TXT, MSE, SCR };
+enum layers { DEF, TXT, MSE, NUM };
+
+enum keycodes {
+    KC_DDS = SAFE_RANGE, // KC_DOT_DOT_SLASH
+};
 
 // Layers
-#define LA_NUML LT(NUM, KC_SPC)
-#define LA_NUMR LT(NUM, KC_BSPC)
 #define LA_MSE LT(MSE, KC_DEL)
 #define LA_TXT LT(TXT, KC_TAB)
-#define LA_SCR MO(SCR)
 
 // Mod-Tap keys
 #define MOD_A LGUI_T(KC_A)
@@ -30,8 +31,6 @@ enum layers { DEF, NUM, TXT, MSE, SCR };
 #define MOD_8 RCTL_T(KC_8)
 #define MOD_9 RALT_T(KC_9)
 #define MOD_0 RGUI_T(KC_0)
-
-#define MOD_SFT LSFT_T(KC_CAPS)
 
 // Shortcuts
 #define SCR_TOP C(KC_HOME)
@@ -58,34 +57,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEF] = LAYOUT_planck_grid(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  MOD_A,   MOD_S,   MOD_D,   MOD_F,   KC_G,    KC_H,    MOD_J,   MOD_K,   MOD_L,   MOD_SCL, KC_QUOT,
-        MOD_SFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_ENT,  LA_TXT,  LA_NUML, LA_NUMR, LA_MSE,  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_ENT,  LA_TXT,  KC_SPC,  KC_BSPC, LA_MSE,  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
     ),
 
     [NUM] = LAYOUT_planck_grid(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_GRV,  MOD_1,   MOD_2,   MOD_3,   MOD_4,   KC_5,    KC_6,    MOD_7,   MOD_8,   MOD_9,   MOD_0,   KC_BSLS,
-        MOD_SFT, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_EQL,  KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, KC_ENT,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-
-    [MSE] = LAYOUT_planck_grid(
-        QK_BOOT, XXXXXXX, XXXXXXX, KC_MS_U, SCR_TOP, XXXXXXX, XXXXXXX, TAB_PRV, TAB_1,   TAB_NXT, XXXXXXX, XXXXXXX,
-        KC_ESC,  XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, LA_SCR,  XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SCR_BOT, XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN5, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        KC_CAPS, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_EQL,  KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, KC_ENT,
+        _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______
     ),
 
-    [SCR] = LAYOUT_planck_grid(
-        QK_BOOT, XXXXXXX, XXXXXXX, KC_WH_U, KC_PGUP, XXXXXXX, XXXXXXX, TAB_PRV, TAB_1,   TAB_NXT, XXXXXXX, XXXXXXX,
-        KC_ESC,  XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, LA_SCR,  XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN5, XXXXXXX, XXXXXXX, XXXXXXX,
+    [MSE] = LAYOUT_planck_grid(
+        QK_BOOT, XXXXXXX, XXXXXXX, KC_MS_U, KC_WH_U, SCR_TOP, XXXXXXX, TAB_PRV, TAB_1,   TAB_NXT, XXXXXXX, XXXXXXX,
+        KC_ESC,  KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN5, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_D, SCR_BOT, XXXXXXX, KC_BTN4, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     [TXT] = LAYOUT_planck_grid(
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_UP,   KC_INS,  KC_PSCR, KC_BSPC,
         KC_ESC,  KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  XXXXXXX,
-        MOD_SFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT,
+        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -121,14 +114,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 #endif
 
-    // Disable caps lock if shift is pressed
-    if (is_shift_pressed(keycode, record) && is_caps_lock_active()) {
-        tap_code(KC_CAPS);
-    }
+    // // Disable caps lock if shift is pressed
+    // if (is_shift_pressed(keycode, record) && is_caps_lock_active()) {
+    //     tap_code(KC_CAPS);
+    // }
 
     // Stay uppercase when caps lock and shift are active
     if (is_caps_lock_active() && is_shift_active() && is_alpha_key(keycode)) {
         unregister_mods(MOD_MASK_SHIFT);
+    }
+
+    switch (keycode) {
+        case KC_DDS:
+            if (record->event.pressed) {
+                SEND_STRING("../");
+            }
+            break;
     }
 
     return true;
@@ -136,8 +137,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        // Hold shift if used quickly
-        case MOD_SFT:
         case LA_TXT:
         case LA_MSE:
             return true;
@@ -148,11 +147,8 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case MOD_SFT:
         case LA_TXT:
         case LA_MSE:
-        case LA_NUML:
-        case LA_NUMR:
             return 0;
         default:
             return QUICK_TAP_TERM;
@@ -165,12 +161,9 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         col = MATRIX_COLS - col - 1;
     }
 
-    switch (keycode) {
-        case LA_NUML:
-        case LA_NUMR:
-            return 1000;
-        default:
-            // Increase tapping term for weaker fingers
-            return TAPPING_TERM + MAX(0, 25 * (col - 1));
-    }
+    return TAPPING_TERM + MAX(0, 25 * (col - 1));
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, TXT, MSE, NUM);
 }
