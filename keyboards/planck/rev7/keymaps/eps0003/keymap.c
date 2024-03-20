308876df31324ad4ce98dbbf9b06de69be9feb49
epsilon <eps0003@users.noreply.github.com>
2024-03-20 20:26:37 +1100

    Add comma to number layer

--- keyboards/planck/rev7/keymaps/eps0003/keymap.c
+++ keyboards/planck/rev7/keymaps/eps0003/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUM] = LAYOUT_planck_grid(
         KC_TAB,  XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, KC_BSPC,
         KC_ESC,  KC_0,    KC_1,    KC_2,    KC_3,    KC_0,    XXXXXXX, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  XXXXXXX,
-        CW_TOGG, XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX, KC_DOT,  XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT,
+        CW_TOGG, XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX, KC_DOT,  KC_COMM, XXXXXXX, XXXXXXX, KC_ENT,
         _______, _______, _______, _______, _______, KC_MINS, _______, _______, _______, _______, _______, _______
     ),
 
