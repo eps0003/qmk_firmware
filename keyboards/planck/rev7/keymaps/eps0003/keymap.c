4b706ea21e30a10a642056ea2c544aa1724c9f53
epsilon <eps0003@users.noreply.github.com>
2025-04-09 22:49:18 +1000

    Restore KC_MINS on NAV layer space key

--- keyboards/planck/rev7/keymaps/eps0003/keymap.c
+++ keyboards/planck/rev7/keymaps/eps0003/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, KC_BSPC,
         KC_ESC,  KC_0,    KC_1,    KC_2,    KC_3,    KC_0,    XXXXXXX, OS_SHFT, OS_CTRL, OS_ALT,  OS_GUI,  XXXXXXX,
         CW_TOGG, KC_GRV,  KC_7,    KC_8,    KC_9,    KC_GRV,  XXXXXXX, KC_DOT,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        _______, _______, _______, _______, _______, _______, KC_MINS, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, KC_MINS, _______, _______, _______, _______, _______, _______
     ),
 
     [FUN] = LAYOUT_planck_grid(
